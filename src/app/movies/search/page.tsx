0bc9766063f263bcc792d908392b10870d1edab3
Fabriola <fabriola@hightao-mg.com>
2024-08-19 12:23:46 +0300

    MAJ

--- src/app/movies/search/page.tsx
+++ src/app/movies/search/page.tsx
@@ -1,5 +1,4 @@
 'use client';
-import { Suspense } from 'react';
 import { MovieService } from '@/Services/movies';
 import { CardMovie } from '@/components/Movies/Card';
 import { Loader } from '@/components/generals/loader';
@@ -8,45 +7,62 @@ import { useSearchParams } from 'next/navigation';
 import { ReactElement, useEffect, useState } from 'react';
 
 export default function Search(): ReactElement {
-    const params = useSearchParams();
-    const keyword: string = params.get('keyword')!;
+	const params = useSearchParams();
+	const keyword: string = params.get('keyword')!;
 
-    const [currentPage, setCurrentPage] = useState(1);
-    const [isLoading, setIsLoading] = useState(true);
-    const [similar, setSimilar] = useState([]);
-    const [films, setFilms] = useState({
-        results: [],
-        total_pages: 500,
-        total_results: 0,
-    });
+	const [currentPage, setCurrentPage] = useState(1);
+	const [isLoading, setIsLoading] = useState(true);
+	const [similar, setSimilar] = useState([]);
+	const [films, setFilms] = useState({
+		results: [],
+		total_pages: 500,
+		total_results: 0,
+	});
 
-    useEffect(() => {
-        setIsLoading(true);
-        MovieService.searchKeyword(keyword)
-            .then((res) => res.json())
-            .then((data) => setSimilar(data.results));
-        
-        MovieService.searchMovie(keyword, currentPage)
-            .then((res) => res.json())
-            .then((data) => {
-                setFilms(data);
-            })
-            .finally(() => setIsLoading(false));
-    }, [params, keyword, currentPage]);
+	useEffect(() => {
+		setIsLoading(true);
+		MovieService.searchKeyword(keyword)
+			.then((res) => res.json())
+			.then((data) => setSimilar(data.results));
+		MovieService.searchMovie(keyword!, currentPage)
+			.then((res) => res.json())
+			.then((data) => {
+				setFilms(data);
+			})
+			.finally(() => setIsLoading(false));
+	}, [params, keyword, currentPage]);
+	return (
+		<>
+			<h1 className='p-4 text-2xl'>
+				Results for {keyword} : {films.total_results} movie(s)
+			</h1>
+			<div className='p-4 text-2xl mb-4'>
+				More to explore :{' '}
+				{similar.map((item: any, index: any) => {
+					return (
+						<span className='capitalize mr-4 hover:text-blue-600' key={index}>
+							<a href={`/movies/search?keyword=${item.name}`}>{item.name}</a> |
+						</span>
+					);
+				})}
+			</div>
+			<Loader isLoading={isLoading} />
+			<div className='flex flex-wrap'>
+				{films.results.map((item: object, index: number) => {
+					return <CardMovie movie={item} key={index} />;
+				})}
+			</div>
 
-    return (
-        <Suspense fallback={<Loader isLoading={isLoading} />}>
-            <h1 className='p-4 text-2xl'>
-                Résultats pour {keyword} : {films.total_results} film(s)
-            </h1>
-            <div className='p-4 text-2xl mb-4'>
-                Plus à explorer :{' '}
-                {similar.map((item: any, index: any) => (
-                    <span className='capitalize mr-4 hover:text-blue-600' key={index}>
-                        <a href={`/movies/search?keyword=${item.name}`}>{item.name}</a> |
-                    </span>
-                ))}
-            </div>
-        </Suspense>
-    );
+			{!isLoading && (
+				<Pagination
+					totalPages={films.total_pages >= 500 ? 500 : films.total_pages}
+					currentPage={currentPage}
+					onPageChange={(pageChange: number) => {
+						setIsLoading(true);
+						setCurrentPage(pageChange);
+					}}
+				/>
+			)}
+		</>
+	);
 }
